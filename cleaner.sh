6aef6966ed49e8e1d6517e43f6df85c6e000d16f
joshuakjacobs <joshuajac007@hotmail.com>
2015-04-22 14:53:58 +0800

    go to root dir first

--- cleaner.sh
+++ cleaner.sh
@@ -1,3 +1,2 @@
-cd ryu/ryu/app/gui_topology;
-
+cd;cd ryu/ryu/app/gui_topology;
 sed -i "s/app_manager.require_app('ryu.app.ofctl_rest')/#app_manager.require_app('ryu.app.ofctl_rest')/g" gui_topology.py
